5f91a19e76cc9b46f64643a5599c9daf69b2b5ba
Damien <ios04@mini-de-ios04.afpa-paris-di>
2016-05-24 12:25:34 +0200

    Première requête iCloud

--- CaptainProf/ViewController.swift
+++ CaptainProf/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CloudKit
 
 class ViewController: UIViewController {
 
@@ -15,6 +16,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         // Ajout comment
         print("Ok")
+        testKit()
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,5 +25,82 @@ class ViewController: UIViewController {
     }
 
 
+    
+    func testKit(){
+        
+        let container = CKContainer(identifier: "iCloud.CAII-CLOUD")
+        //CKContainer.defaultContainer()
+        var publicDB : CKDatabase?
+        var curRecord : CKRecord?
+        
+        publicDB = container.publicCloudDatabase
+        
+       //let predicate = NSPredicate(format: "nom = %@", "Doe")
+        let predicate = NSPredicate(value: true)
+        let query = CKQuery(recordType: "Etudiant", predicate: predicate)
+        
+        //let predicate = NSPredicate(value: true)
+        //let query = CKQuery(recordType: "Etudiant", predicate: predicate)
+        publicDB!.performQuery(query, inZoneWithID: nil) { results, error in
+            if error != nil {
+                dispatch_async(dispatch_get_main_queue()) {
+                    print("error loading: \(error)")
+                }
+            } else {
+                
+                for record in results! {
+                    //let etudiant = Etudiant(record: record , database: publicDB!)
+                    //let etudiant = Etudiant(record: record as! CKRecord, database: self.publicDB)
+                    print("nom", record.objectForKey("nom") as! String)
+                    print("prenom", record.objectForKey("prenom") as! String)
+                    
+                }
+                dispatch_async(dispatch_get_main_queue()) {
+                    
+                    print("")
+                }
+            }
+        }
+        
+        
+    }
+
+    
+
+        func loadItems() {
+            let container = CKContainer(identifier: "iCloud.CAII-CLOUD")
+            //CKContainer.defaultContainer()
+            var publicDB : CKDatabase?
+            var curRecord : CKRecord?
+            publicDB = container.publicCloudDatabase
+            
+            let predicate = NSPredicate(value: true)
+            let query = CKQuery(recordType: "Etudiant", predicate: predicate)
+            publicDB!.performQuery(query, inZoneWithID: nil) { (results, error) -> Void in
+                if error != nil {
+                    print(error!.localizedDescription)
+                }
+                
+                dispatch_async(dispatch_get_main_queue()) { () -> Void in
+                    let items = results! as [CKRecord]
+                    //self.tableView.reloadData()
+                    //print("nom: \(items)")
+                    
+                    for record in results! {
+                        //let etudiant = Etudiant(record: record , database: publicDB!)
+                        //let etudiant = Etudiant(record: record as! CKRecord, database: self.publicDB)
+                        
+                        print("Record %@", record.objectForKey("nom") as! String)
+ 
+                        
+                    }
+
+                    
+                    
+                }
+            }
+        }
+
+    
 }
 
