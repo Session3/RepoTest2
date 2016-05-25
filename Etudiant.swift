34a5581fcc8918ea96032ccc0bd008fff3b48604
Damien <ios04@mini-de-ios04.afpa-paris-di>
2016-05-25 16:53:23 +0200

    Creation Class Etudiant

---
+++ Etudiant.swift
@@ -0,0 +1,25 @@
+//
+//  Etudiant.swift
+//  CaptainProf
+//
+//  Created by IOS04 on 25/05/2016.
+//  Copyright © 2016 CEDRIC. All rights reserved.
+//
+
+import UIKit
+import CloudKit
+
+class Etudiant: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        // Ajout comment
+        print("etudiant")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
